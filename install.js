ce156481dee139cce03c06975d5eca5749c4795e
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2018-05-21 23:02:13 +1200

    added cli features

---
+++ install.js
@@ -0,0 +1,87 @@
+const chalk = require('chalk')
+const figlet = require('figlet')
+const fs = require('fs')
+const util = require('util')
+
+
+
+const questions = [
+  { phrase: "Title? ", configName: "title"},
+  { phrase: "src directory? ", configName: "src"},
+  { phrase: "dist directory? ", configName: "dist"},
+  { phrase: "config path (ink. name.json) ", configName: "configPath"},
+]
+
+let questionIndex = 0
+
+const config = {}
+
+function ask(questions, index) {
+  process.stdout.write(questions[index].phrase)
+}
+
+function quesion(index) {
+
+}
+
+function printConfigs(questions) {
+  console.log(chalk.yellow('Following settings can be changed at any time in ') + chalk.red(config.configPath))
+  Object.entries(config).forEach( (a) => {
+    process.stdout.write(`${a[0]} : ${a[1]} \n`)
+  })
+}
+
+function handleQA(){
+  ask( questions, questionIndex)
+}
+
+function saveConfig( str ) {
+  config[ questions[ questionIndex ].configName ] = str.trim()
+}
+
+function saveToFile() {
+  const path = `${process.cwd()}/${config.configPath}`;
+  if (!fs.existsSync(path)) {
+      console.log('Createing new folder', path)
+      fs.mkdirSync(`${path}`, 0744); // with read and write permission
+  }
+  fs.writeFileSync(`${path}/rsg-config.json`, JSON.stringify(config, null, 2) , 'utf-8');
+}
+
+process.stdin.on('data', (data) => {
+  saveConfig( data.toString() )
+  if ( questionIndex >= questions.length - 1) {
+    printConfigs()
+    saveToFile()
+    process.exit()
+  }
+  else{
+    questionIndex += 1
+    handleQA()
+
+  }
+})
+
+figlet('Ricks Style Guide!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(chalk.yellow(data));
+    handleQA()
+});
+
+
+// console.log('PATH:', process.cwd() )
+
+// var path = require('path');
+// var appDir = path.dirname(require.main.filename);
+// console.log(appDir)
+// console.log(process.argv)
+
+
+
+
+
+
